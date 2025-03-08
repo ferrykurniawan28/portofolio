4c9649fa2199461d51da19735298b23aac75888f
ferrykurniawan28 <147892622+ferrykurniawan28@users.noreply.github.com>
2025-03-08 19:46:02 +0700

    test update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2615738032"
+    serviceWorkerVersion: "3373241537"
   }
 });
